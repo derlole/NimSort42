139b6c6d0d701a662c4f82978d0039191c79efe9
yaba-source <12479yp@gmx.de>
2026-04-07 21:42:00 +0200

    feat :mutally exclusive callback

--- ros_ws/src/nimsort_nodes/nimsort_nodes/main_node.py
+++ ros_ws/src/nimsort_nodes/nimsort_nodes/main_node.py
@@ -1,7 +1,5 @@
 import rclpy
-from rclpy.node import Node 
-import time
-import threading
+from rclpy.node import MutuallyExclusiveCallbackGroup, Node 
 from nimsort_logic.nimsort_main.main_interface import MainInterface
 from nimsort_logic.nimsort_main.main import NimSortMain 
 from ro45_portalrobot_interfaces.msg import MotionState, Target, Prediction
@@ -12,37 +10,39 @@ class MainNode(Node):
         
         self.nimsort_main = NimSortMain()
         self.publisher_ = self.create_publisher(Target, 'target', 10)
-        self.callback_lock = threading.Lock()
+        
+       
+        self.callback_group = MutuallyExclusiveCallbackGroup()
 
+        
         self.subscription_motion = self.create_subscription(
             MotionState,
             'motion_state',
             self.listener_callback_motion,
-            10)
+            10,
+            callback_group=self.callback_group)
         
         self.subscription_prediction = self.create_subscription(
             Prediction,
             'prediction',
             self.listener_callback_prediction,
-            10)
+            10,
+            callback_group=self.callback_group)
 
     def listener_callback_motion(self, msg):
-        thread = threading.Thread(target=self._process_motion, args=(msg,))
-        thread.start()
+        """Verarbeitet MotionState Nachricht"""
+        self.nimsort_main.process_motion_state(msg)
+        self.get_logger().debug(f'MotionState verarbeitet. State: {self.nimsort_main.get_current_state()}')
     
     def listener_callback_prediction(self, msg):
-        thread = threading.Thread(target=self._process_prediction, args=(msg,))
-        thread.start()
-    
-    def _process_motion(self, msg):
-        with self.callback_lock:
-            target_msg = self.nimsort_main.compute_target(msg)
-            self.publisher_.publish(target_msg)
-    
-    def _process_prediction(self, msg):
-        with self.callback_lock:
-            target_msg = self.nimsort_main.compute_target(msg)
-            self.publisher_.publish(target_msg)
+        """Verarbeitet Prediction Nachricht und veröffentlicht Target"""
+        target = self.nimsort_main.process_prediction(msg)
+        
+        if target is not None:
+            self.publisher_.publish(target)
+            self.get_logger().info(f'Target veröffentlicht. State: {self.nimsort_main.get_current_state()}')
+        else:
+            self.get_logger().debug(f'Keine Target für Prediction. State: {self.nimsort_main.get_current_state()}')
 
 def main(args=None):
     rclpy.init(args=args)
@@ -51,7 +51,7 @@ def main(args=None):
     
     main_node = MainNode()
     
-    # MultiThreadedExecutor für parallele Callback-Verarbeitung
+    
     executor = MultiThreadedExecutor()
     executor.add_node(main_node)
     
@@ -66,4 +66,4 @@ def main(args=None):
 
 
 if __name__ == '__main__':
-    main() s 
+    main() 
