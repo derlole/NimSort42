e7db19f56cf5090c4b9c552c413643c98f76f7ad
derlole <benjaaimnpekkler@gmail.com>
2026-04-08 12:48:57 +0200

    chore(main_interface.py): removed I's in function names

--- nimsort_logic/nimsort_main/main_interface.py
+++ nimsort_logic/nimsort_main/main_interface.py
@@ -8,26 +8,26 @@ class MainInterface(ABC):
     """
     
     @abstractmethod
-    def Iprocess_motion_state(self, motion_state) -> None:
+    def process_motion_state(self, motion_state) -> None:
         """
         Verarbeitet den Zustand der Motion und aktualisiert internen Zustand.
         """
         pass
     
     @abstractmethod
-    def Iprocess_prediction(self, prediction) -> object:
+    def process_prediction(self, prediction) -> object:
         """
         Verarbeitet Prediction von Vision.
         """
         pass
     
     @abstractmethod
-    def Iget_current_state(self) -> str:
+    def get_current_state(self) -> str:
         """Gibt aktuellen State-Machine-Zustand zurück"""
         pass
     
     @abstractmethod
-    def Ireset(self) -> None:
+    def reset(self) -> None:
         """Setzt State Machine zurück"""
         pass
 
