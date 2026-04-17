e0dcbb5be1f8ca14f57655585c9d7822dece269d
derlole <benjaaimnpekkler@gmail.com>
2026-04-17 23:42:44 +0200

    feat: issue Schwerpunkt- in wcs transformieren und auf topic legen Fixes #60

--- ros_ws/src/nimsort_nodes/nimsort_nodes/camera_supreme_commander.py
+++ ros_ws/src/nimsort_nodes/nimsort_nodes/camera_supreme_commander.py
@@ -2,9 +2,10 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
 from tf2_ros import Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PointStamped, Point
 
 from nimsort_msgs.msg import NimSortImageData
+from nimsort_vision.opencv_pipeline import OpencvPipeline
 
 WORLD_FRAME = "world"
 ROBOT_FRAME = "robot"
@@ -15,15 +16,16 @@ class Vision(Node):
     def __init__(self):
         super().__init__('camera_supreme_commander')
 
-        self.publisher_ = self.create_publisher(
+        self.image_data_publisher = self.create_publisher(
             NimSortImageData, 
             '/NimSortImageData', 
             10
         )
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        
         self.timer = self.create_timer(0.1, self.main_order)
+
+        self.pipeline = OpencvPipeline()
 
     def tf_camera_to_world(self, x: float, y: float, z: float) -> PointStamped | None:
         """ transforms one point from camera-coordinatesystem into worl-coordinatesystem"""
@@ -46,12 +48,33 @@ class Vision(Node):
         except(LookupException, ConnectivityException, ExtrapolationException) as e:
             self.get_logger().warn(f"{str(e)}")
             return None
+        
+    def publish_image_data(self, x_ccs: float, y_ccs: float, z_ccs: float, ts: int, object_type: int, conveyor_belt_speed: float):
+        msg = NimSortImageData()
+        msg.current_position_wcs = Point()
+
+        msg.current_position_wcs.x = x_ccs
+        msg.current_position_wcs.y = y_ccs
+        msg.current_position_wcs.z = z_ccs
+        msg.object_type = object_type
+        msg.ts = ts
+        msg.conveyor_belt_speed = conveyor_belt_speed 
+
+        self.image_data_publisher.publish(msg)
+
 
     def main_order(self):
-        pass
+        try:
+            self.pipeline.captureImage()
+            x_ccs, y_ccs, z_ccs, ts, image = self.pipeline.getImageData()
 
+        except ValueError as e:
+            self.get_logger.error("[ACN-][main_ord]:" + str(e))
 
+        # TODO insert trained_model_here to calculate the correct object_type
+        point_wcs = self.tf_camera_to_world(x_ccs, y_ccs, z_ccs)
 
+        self.publish_image_data(point_wcs.point.x, point_wcs.point.y, point_wcs.point.z, ts, 1, 0.01) # TODO repalce the consants at the time zou have them
 
 def main(args=None):
     rclpy.init(args=args)
