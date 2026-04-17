3d7e7098006c687ab98f8643eb5a65f5be05d8fa
derlole <benjaaimnpekkler@gmail.com>
2026-04-17 22:33:02 +0200

    feat: transform in Vision node

--- ros_ws/src/nimsort_nodes/nimsort_nodes/camera_supreme_commander.py
+++ ros_ws/src/nimsort_nodes/nimsort_nodes/camera_supreme_commander.py
@@ -1,10 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
+from tf2_ros import Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException
+from geometry_msgs.msg import PointStamped
 
 from nimsort_msgs.msg import NimSortImageData
 
-
+WORLD_FRAME = "world"
+ROBOT_FRAME = "robot"
+CAMERA_FRAME = "camera"
 
 class Vision(Node):
     
@@ -16,8 +20,32 @@ class Vision(Node):
             '/NimSortImageData', 
             10
         )
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
         
         self.timer = self.create_timer(0.1, self.main_order)
+
+    def tf_camera_to_world(self, x: float, y: float, z: float) -> PointStamped | None:
+        """ transforms one point from camera-coordinatesystem into worl-coordinatesystem"""
+        point = PointStamped()
+        point.header.frame_id = CAMERA_FRAME
+        point.header.stamp = self.get_clock().now().to_msg()
+
+        point.point.x = x
+        point.point.y = y
+        point.point.z = z
+
+        try:
+            transformed = self.tf_buffer.transform(
+                point,
+                WORLD_FRAME,
+                timeout=rclpy.duration.Duration(seconds=0.05)
+            )
+            return transformed
+        
+        except(LookupException, ConnectivityException, ExtrapolationException) as e:
+            self.get_logger().warn(f"{str(e)}")
+            return None
 
     def main_order(self):
         pass
