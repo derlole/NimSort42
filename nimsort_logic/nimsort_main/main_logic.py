63274360170ed084e6a079b8c05595d328d82209
derlole <benjaaimnpekkler@gmail.com>
2026-05-11 11:12:34 +0200

    chore: test small drive

--- nimsort_logic/nimsort_main/main_logic.py
+++ nimsort_logic/nimsort_main/main_logic.py
@@ -5,6 +5,7 @@ from nimsort_vision.plausibility_check import PlausibilityCheck
 
 POSITION_UNCORN= [-0.16,-0.14, 0.07]
 POSITION_CAT= [-0.06, -0.14, 0.07]
+POSITION_ONE= [-0.01, -0.01, 0.01]
 
 Z_PRE_POST_PICK= 5.0 #z-Höhe über Objekt für Pick-Preposition
 Z_PICK=10.0 #z-Höhe über Objekt für Pick-Position
