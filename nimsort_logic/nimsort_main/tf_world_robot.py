5c20adb37f536282af79977e0fadc5e453c29742
derlole <benjaaimnpekkler@gmail.com>
2026-05-27 12:15:27 +0200

    feat: extended transforming and fixed missing transform in config_main

--- nimsort_logic/nimsort_main/tf_world_robot.py
+++ nimsort_logic/nimsort_main/tf_world_robot.py
@@ -8,3 +8,27 @@ class TransformWorldRobot:
     @staticmethod
     def robot_to_world( x_r: float, y_r: float, z_r: float) -> tuple[float, float, float]:
         return (x_r + WORLD_TO_ROBOT_TRANSLATION[0], y_r + WORLD_TO_ROBOT_TRANSLATION[1], z_r + WORLD_TO_ROBOT_TRANSLATION[2])
+    
+    @staticmethod
+    def robot_to_world_z( z_r: float) -> float:
+        return z_r + WORLD_TO_ROBOT_TRANSLATION[2]
+    
+    @staticmethod
+    def world_to_robot_z( z_w: float) -> float:
+        return z_w - WORLD_TO_ROBOT_TRANSLATION[2]
+    
+    @staticmethod
+    def world_to_robot_x( x_w: float) -> float:
+        return x_w - WORLD_TO_ROBOT_TRANSLATION[0]
+    
+    @staticmethod
+    def world_to_robot_y( y_w: float) -> float:
+        return y_w - WORLD_TO_ROBOT_TRANSLATION[1]
+    
+    @staticmethod
+    def robot_to_world_x( x_r: float) -> float:
+        return x_r + WORLD_TO_ROBOT_TRANSLATION[0]
+    
+    @staticmethod
+    def robot_to_world_y( y_r: float) -> float:
+        return y_r + WORLD_TO_ROBOT_TRANSLATION[1]
