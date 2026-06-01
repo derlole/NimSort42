d09ad93ff3147d9dbc5e705d34138cb4f80f295e
derlole <benjaaimnpekkler@gmail.com>
2026-06-01 10:28:40 +0200

    feat: transform every value

--- nimsort_logic/configs/config_main.py
+++ nimsort_logic/configs/config_main.py
@@ -1,15 +1,14 @@
 from nimsort_main.tf_world_robot import TransformWorldRobot
 
 # main_logic
-POSITION_UNCORN: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.16,-0.14, 0.07)
-POSITION_CAT: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.06, -0.14, 0.07)
-INITIAL_POSITION: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.01,-0.05, 0.02)
-Z_PRE_POST_PICK: float = 0.08 
+POSITION_UNCORN: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.165,-0.135, 0.07)
+POSITION_CAT: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.065, -0.135, 0.07)
+INITIAL_POSITION: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.005,-0.074, 0.02)
+Z_PRE_POST_PICK: float = 0.08
 Z_PICK: float = TransformWorldRobot.robot_to_world_z(0.095) 
-GENERIC_PICK_PRE_POSITION: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.01, -0.05, Z_PRE_POST_PICK)
+GENERIC_PICK_PRE_POSITION: tuple[float, float, float] = TransformWorldRobot.robot_to_world(-0.01, -0.074, Z_PRE_POST_PICK)
 SENTINEL: tuple[float, float, float] = (-1.0, -1.0, -1.0,-1)
-ROBOT_REACH: float = -0.3
+ROBOT_REACH: float = TransformWorldRobot.robot_to_world_x(-0.26)
 
-# TF_CONFIG
 WORLD_TO_ROBOT_TRANSLATION: tuple[float, float, float] = (0.29, -0.04, 0.083) # muss das ncht ein Tupple sein? 0.083 unsicher 
-
+ZERO_ROBOT_POSITION: tuple[float, float, float] = TransformWorldRobot.robot_to_world(0.0, 0.0, 0.0)
