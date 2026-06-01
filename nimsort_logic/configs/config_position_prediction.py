47dd6ab42480d1825d9fdef169777115cd11224b
derlole <benjaaimnpekkler@gmail.com>
2026-06-01 13:47:01 +0200

    chore: config update

--- nimsort_logic/configs/config_position_prediction.py
+++ nimsort_logic/configs/config_position_prediction.py
@@ -4,6 +4,7 @@ X_THRESHOLD = 0.55
 DUPLICATE_THRESHOLD = 0.06  # Maximaler Abstand in X, um Objekte als Duplikate zu betrachten
 SENTINEL_POSITION = (-1.0, -1.0, -1.0)
 SENTINEL_TYPE = -1
+PREDICTION_PUBLISH_THRESHOLD = 0.28
 
 # position_prediction_node
 DEFAULT_CONVEYOR_BELT_SPEED = 0.01
